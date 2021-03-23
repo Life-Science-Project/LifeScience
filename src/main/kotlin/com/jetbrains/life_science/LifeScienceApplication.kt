9b36bc75da598db4cfd67859a856816716730acc
qwake1378 <45040429+qwake1378@users.noreply.github.com>
2021-03-23 23:53:43 +0300

    Initial commit

---
+++ src/main/kotlin/com/jetbrains/life_science/LifeScienceApplication.kt
@@ -0,0 +1,11 @@
+package com.jetbrains.life_science
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LifeScienceApplication
+
+fun main(args: Array<String>) {
+    runApplication<LifeScienceApplication>(*args)
+}
