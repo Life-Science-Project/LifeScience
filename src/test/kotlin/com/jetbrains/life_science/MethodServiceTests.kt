95d0e863a0bfb347161f53eb244f5714ebeb615a
Kirill Peshkov <kirill906@yandex.ru>
2021-03-26 22:38:08 +0300

    Sample tests added

---
+++ src/test/kotlin/com/jetbrains/life_science/MethodServiceTests.kt
@@ -0,0 +1,97 @@
+package com.jetbrains.life_science
+
+import com.jetbrains.life_science.article.dto.ArticleDTO
+import com.jetbrains.life_science.article.entity.Article
+import com.jetbrains.life_science.article.entity.ArticleInfo
+import com.jetbrains.life_science.article.service.ArticleServiceImpl
+import com.jetbrains.life_science.exceptions.MethodNotFoundException
+import com.jetbrains.life_science.method.dto.MethodDTO
+import com.jetbrains.life_science.method.dto.MethodDTOToInfoWrapper
+import com.jetbrains.life_science.method.entity.Method
+import com.jetbrains.life_science.method.repository.MethodRepository
+import com.jetbrains.life_science.method.service.MethodService
+import com.jetbrains.life_science.section.entity.Section
+import com.jetbrains.life_science.section.service.SectionServiceImpl
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+import org.mockito.ArgumentMatchers.*
+import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+
+@SpringBootTest
+class MethodServiceTests @Autowired constructor(val methodService: MethodService) {
+
+    private fun <T> any(type: Class<T>): T = Mockito.any(type)
+
+    @MockBean
+    lateinit var methodRepository : MethodRepository
+
+    @MockBean
+    lateinit var articleService : ArticleServiceImpl
+
+    @MockBean
+    lateinit var sectionService: SectionServiceImpl
+
+    @Test
+    fun methodAdd() {
+        Mockito
+            .`when`(articleService.addArticle(any(ArticleInfo::class.java)))
+            .thenReturn(Article(666))
+
+        Mockito
+            .`when`(sectionService.getSection(anyLong()))
+            .thenReturn(Section(666, "Test session"))
+
+        val methodInfo = MethodDTOToInfoWrapper(MethodDTO("Method #21321", 1, ArticleDTO()))
+
+        methodService.addMethod(methodInfo)
+    }
+
+    @Test
+    fun methodGet() {
+        val section = Section(16, "Section you got")
+        val article = Article(45)
+        val method = Method(12, "Method you got", section, article)
+
+        Mockito
+            .`when`(methodRepository.existsById(666))
+            .thenReturn(false)
+
+        Mockito
+            .`when`(methodRepository.existsById(12))
+            .thenReturn(true)
+
+        Mockito
+            .`when`(methodRepository.getOne(12))
+            .thenReturn(method)
+
+        assertThatThrownBy{methodService.getMethod(666)}.isInstanceOf(MethodNotFoundException::class.java)
+        assertThat(methodService.getMethod(12)).isEqualTo(method)
+
+    }
+
+    @Test
+    fun methodDelete() {
+        val section = Section(1, "first section")
+        val article = Article(1)
+        val method = Method(1, "First method", section, article)
+
+        Mockito
+            .`when`(methodRepository.existsById(666))
+            .thenReturn(false)
+
+        Mockito
+            .`when`(methodRepository.existsById(1))
+            .thenReturn(true)
+
+        Mockito
+            .`when`(methodRepository.getOne(1))
+            .thenReturn(method)
+
+        assertThatThrownBy{methodService.deleteMethod(666)}.isInstanceOf(MethodNotFoundException::class.java)
+        methodService.deleteMethod(1)
+    }
+}
