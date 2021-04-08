eb6e59ca5f8ed974f1c9e9e2abb00e72feb32b08
Andrey <andreystar2403@gmail.com>
2021-04-08 22:13:36 +0300

    Update gradle config

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -24,11 +24,14 @@ repositories {
 }
 
 dependencies {
-    runtimeOnly(project(":frontend"))
+    implementation(project(":frontend"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.jsonwebtoken:jjwt:0.9.0")
+    implementation("io.jsonwebtoken:jjwt-api:0.10.6")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
