d59709a7aa2d7c7df19123d506df107edbabcb5a
avpotapov00 <aleksandrpotapov955@gmail.com>
2021-04-13 18:26:00 +0300

    tests fixed

--- backend/src/test/resources/scripts/test_trunc_data.sql
+++ backend/src/test/resources/scripts/test_trunc_data.sql
@@ -1,3 +1,7 @@
 truncate table container cascade;
+truncate table method_version cascade ;
 truncate table method cascade;
 truncate table section cascade;
+truncate table users_roles;
+truncate table users cascade ;
+truncate table roles cascade ;
