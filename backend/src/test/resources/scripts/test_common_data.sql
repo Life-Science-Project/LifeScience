a6eab5ad998d4c510d0fd82508b5be153c0e1a79
avpotapov00 <aleksandrpotapov955@gmail.com>
2021-04-17 09:27:06 +0300

    test scripts fixed

--- backend/src/test/resources/scripts/test_common_data.sql
+++ backend/src/test/resources/scripts/test_common_data.sql
@@ -10,10 +10,10 @@ values (1, 1);
 insert into article (id, category_id)
 values (2, 2);
 
-insert into users (id, email, enabled, first_name, last_name, password, username)
-VALUES (1, 'e', true, 'test user name', 'test user last_name', 'password', 'sample');
-insert into users (id, email, enabled, first_name, last_name, password, username)
-VALUES (2, 'e', true, 'test user 2 name', 'test user 2 last_name', 'password2', 'sample2');
+insert into user_credential (id, password, username)
+VALUES (1, 'password', 'sample');
+insert into user_credential (id, password, username)
+VALUES (2, 'password2', 'sample2');
 
 insert into article_version (id, name, state, author_id, main_article_id)
 values (1, 'master 1', 3, 1, 1);
