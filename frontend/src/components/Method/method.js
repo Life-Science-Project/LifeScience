aa9b5787fb9343747c140882b649bba10f7d461f
Denis Reznichenko <Denis.Reznichenko@jetbrains.com>
2021-04-19 19:36:52 +0300

    moved files

--- frontend/src/components/Method/method.js
+++ frontend/src/components/Method/method.js
@@ -7,21 +7,33 @@ import {
     NavLink,
     Redirect
 } from "react-router-dom";
-// import {Redirect} from "react-router";
+import GeneralInformation from "./Sections/GeneralInformation/general-information";
+import Protocol from "./Sections/Protocol/protocol";
+import Application from "./Sections/Application/application";
+import Advantages from "./Sections/AdvantagesDisadvantages/advantages-disadvantages";
+import Collaboration from "../FindCollaboration/collaboration";
+
+const sectionFunctions = [
+    (section) => (<GeneralInformation {...section}/>),
+    (section) => (<Protocol {...section}/>),
+    (section) => (<Application {...section}/>),
+    (section) => (<Advantages {...section}/>),
+    (section) => (<Collaboration {...section}/>)
+]
 
 const Method = ({link, name, sections}) => {
     return (
         <Router>
             <div className="method-name">
-                <h3>
+                <h2>
                     {name}
-                </h3>
+                </h2>
             </div>
             <div className="main">
                 <ul className="section-list">
                     {
                         sections.map((section) => (
-                            <li className="section-list-item">
+                            <li className="list-item">
                                 <NavLink exact activeClassName="active-section" className="section-link"
                                          to={section.link}>{section.name}</NavLink>
                             </li>
@@ -38,8 +50,7 @@ const Method = ({link, name, sections}) => {
                         sections.map((section, index) => (
                             <Route exact={index === 0} path={section.link}>
                                 {
-                                    <div className="section-content"
-                                         dangerouslySetInnerHTML={{__html: section.content}}/>
+                                    sectionFunctions[index](section)
                                 }
                             </Route>
                         ))
@@ -60,20 +71,37 @@ Method.defaultProps = {
         {
             link: "/bradford-assay/general",
             name: "General Information",
-            content: "<div>Method for quantify the protein content in sample. This method has multiple applications in experimental sciences. <a href='./general/some'>Chemical basis</a> of the Bradford method (1976) is based on the absorbance shift observed in an acidic solution of dye Coomassie® Brilliant Blue G-250. When added to a solution of protein, the dye binds to the protein resulting in a color change from a reddish brown to blue.</div>"
+            content: "<div>Method for quantify the protein content in sample. This method has multiple applications in experimental sciences. <a href='/chemical-basis'>Chemical basis</a> of the Bradford method (1976) is based on the absorbance shift observed in an acidic solution of dye Coomassie® Brilliant Blue G-250. When added to a solution of protein, the dye binds to the protein resulting in a color change from a reddish brown to blue.</div>",
+            refs: ["Bradford MM A rapid and sensitive method for the quantitation of microgram quantities of protein utilizing the principle of protein-dye binding. // Analytical Biochemistry. 1976. № 72. С. 248-254.",
+                "Pedrol, Nuria & Tamayo, Pilar. (2001). Protein Content Quantification by Bradford Method. 10.1007/0-306-48057-3_19. "]
         },
         {
             link: "/bradford-assay/protocol",
             name: "Protocol",
-            content: "<div>Curabitur tempus felis eu diam tincidunt porttitor. Aenean at dui nisi. Phasellus et dictum mi. Nulla et ex vitae dolor vulputate dignissim. Nunc nulla libero, commodo nec odio vel, blandit tincidunt tellus. Nullam porttitor mattis accumsan. Nullam bibendum risus at lorem ultricies lobortis. Fusce et laoreet lorem.\n" +
-                "\n" +
-                "Duis at malesuada mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam erat volutpat. Sed euismod dui vitae ligula rutrum, vitae fringilla urna luctus. Fusce sit amet risus vel ipsum tincidunt sagittis eu eget quam. Nullam tincidunt lorem augue, et bibendum nisi pretium at. Proin at eleifend arcu. Pellentesque dolor dui, aliquet eget dui eu, laoreet egestas dui. Sed eros tellus, lobortis ut aliquet eu, consectetur eget est. Curabitur consequat tellus arcu, at aliquet lacus posuere ac. Vestibulum tempor ut orci et laoreet. Curabitur eleifend euismod dapibus. Mauris sit amet massa ipsum. Nulla maximus turpis quis elit congue, non sodales augue euismod. Integer in congue enim.</div>"
+            content: "too hard for me right now"
         },
         {
-            link: "/bradford-assay/trouble",
-            name: "Troubleshooting",
-            content: "Troubleshooting information"
+            link: "/bradford-assay/application",
+            name: "Application",
+            content: ["<a href='/link1'>Quantitative analysis of proteins</a>",
+                "<a href='/link2'>Some other application</a>"],
+        },
+        {
+            link: "/bradford-assay/advantages",
+            name: "Advantages and disadvantages",
+            content: [
+                ["by whom / parameter", "by Admin1", "by Admin2", "by Admin3"],
+                ["Cost", "$1 per gram", "$1.5 per gram", ""],
+                ["Time", "2 hours", "2.5 hours", "120 minutes"],
+                ["Sensitivity", "High", "", "Very High"]
+            ]
+        },
+        {
+            link: "/bradford-assay/collaboration",
+            name: "Find collaboration",
+            content: {}
         }
+
     ]
 }
 
