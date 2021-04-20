5bca2bca19a05a30e61742c3ace6d16d8135fb87
Andrey <andreystar2403@gmail.com>
2021-04-20 13:54:22 +0300

    Remove duplicate cors configurer

--- backend/src/main/kotlin/com/jetbrains/life_science/LifeScienceApplication.kt
+++ backend/src/main/kotlin/com/jetbrains/life_science/LifeScienceApplication.kt
@@ -2,23 +2,10 @@ package com.jetbrains.life_science
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class LifeScienceApplication
 
 fun main(args: Array<String>) {
     runApplication<LifeScienceApplication>(*args)
-}
-
-@Bean
-fun corsConfigurer(): WebMvcConfigurer {
-    return object : WebMvcConfigurer {
-        override fun addCorsMappings(registry: CorsRegistry) {
-            registry.addMapping("**").allowedOrigins("http://localhost:3000")
-        }
-    }
 }
