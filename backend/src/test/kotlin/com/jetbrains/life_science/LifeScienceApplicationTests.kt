7d2f4f71820b4229772858262dbe1daf12c03653
avpotapov00 <aleksandrpotapov955@gmail.com>
2021-04-23 01:55:26 +0300

    tests fix

--- backend/src/test/kotlin/com/jetbrains/life_science/LifeScienceApplicationTests.kt
+++ backend/src/test/kotlin/com/jetbrains/life_science/LifeScienceApplicationTests.kt
@@ -1,10 +1,15 @@
 package com.jetbrains.life_science
 
+import com.jetbrains.life_science.article.content.version.config.ContentVersionConfig
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 
 @SpringBootTest
 class LifeScienceApplicationTests {
+
+    @MockBean
+    lateinit var contentVersionConfig: ContentVersionConfig
 
     @Test
     fun contextLoads() {
