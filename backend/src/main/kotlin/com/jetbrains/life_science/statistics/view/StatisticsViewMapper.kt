90a908682dfc8f00af63ce9edd2ba931ffbae5cd
avpotapov00 <aleksandrpotapov955@gmail.com>
2021-05-11 03:15:50 +0300

    organisations count added to home statistics view

--- backend/src/main/kotlin/com/jetbrains/life_science/statistics/view/StatisticsViewMapper.kt
+++ backend/src/main/kotlin/com/jetbrains/life_science/statistics/view/StatisticsViewMapper.kt
@@ -1,11 +1,16 @@
 package com.jetbrains.life_science.statistics.view
 
+import com.jetbrains.life_science.statistics.entity.Statistics
 import org.springframework.stereotype.Component
 
 @Component
 class StatisticsViewMapper {
 
-    fun toView(usersCount: Long, articlesCount: Long): StatisticsView {
-        return StatisticsView(usersCount, articlesCount)
+    fun toView(statistics: Statistics): StatisticsView {
+        return StatisticsView(
+            usersCount = statistics.usersCount,
+            articlesCount = statistics.articlesCount,
+            organisationsCount = statistics.organisationsCount
+        )
     }
 }
