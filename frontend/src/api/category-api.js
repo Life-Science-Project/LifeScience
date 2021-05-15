35089cfdfa33f81d3d65eeaaad839113426b3db1
”Dmitri <dmitri.zd1958@gmail.com>
2021-05-15 16:22:37 +0300

    Category api changed

--- frontend/src/api/category-api.js
+++ frontend/src/api/category-api.js
@@ -4,5 +4,17 @@ export const categoryApi = {
     getCategory(id) {
         const url = 'categories/' + (id === undefined ? 1 : id);
         return instance.get(url)
+    },
+    putCategory(id, data) {
+        const url = `categories/${id}`;
+        return instance.put(url, data);
+    },
+    deleteCategory(id) {
+        const url = `categories/${id}`;
+        return instance.delete(url);
+    },
+    postCategory(data) {
+        const url = `categories`;
+        return instance.post(url, data);
     }
 }
