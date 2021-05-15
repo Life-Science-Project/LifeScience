f280c93a5006d66874ab2676c09bb15fed77ef33
avpotapov00 <aleksandrpotapov955@gmail.com>
2021-05-15 12:13:02 +0300

    ktlint fix

--- backend/src/main/kotlin/com/jetbrains/life_science/article/section/controller/SectionController.kt
+++ backend/src/main/kotlin/com/jetbrains/life_science/article/section/controller/SectionController.kt
@@ -5,7 +5,6 @@ import com.jetbrains.life_science.article.section.dto.SectionDTOToInfoAdapter
 import com.jetbrains.life_science.article.section.service.SectionService
 import com.jetbrains.life_science.article.section.view.SectionView
 import com.jetbrains.life_science.article.section.view.SectionViewMapper
-import com.jetbrains.life_science.article.version.entity.State
 import com.jetbrains.life_science.article.version.service.ArticleVersionService
 import com.jetbrains.life_science.exception.UnauthorizedException
 import com.jetbrains.life_science.exception.not_found.SectionNotFoundException
