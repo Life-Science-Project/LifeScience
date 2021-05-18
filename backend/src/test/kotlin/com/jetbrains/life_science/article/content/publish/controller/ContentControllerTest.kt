bfd05c832a9bf6a04a16a8db866d18f4e442d8be
avpotapov00 <aleksandrpotapov955@gmail.com>
2021-05-18 15:56:54 +0300

    ktlint format

--- backend/src/test/kotlin/com/jetbrains/life_science/article/content/publish/controller/ContentControllerTest.kt
+++ backend/src/test/kotlin/com/jetbrains/life_science/article/content/publish/controller/ContentControllerTest.kt
@@ -13,7 +13,6 @@ import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
