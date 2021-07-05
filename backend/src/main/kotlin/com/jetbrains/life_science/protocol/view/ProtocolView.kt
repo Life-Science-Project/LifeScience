6e2419abed6096d8e6bc33b1c0b63ca754ad33d0
Kirill Peshkov <kirill906@yandex.ru>
2021-07-05 17:06:09 +0300

    ProtocolSectionView -> SectionView в одном месте

--- backend/src/main/kotlin/com/jetbrains/life_science/protocol/view/ProtocolView.kt
+++ backend/src/main/kotlin/com/jetbrains/life_science/protocol/view/ProtocolView.kt
@@ -1,10 +1,10 @@
 package com.jetbrains.life_science.protocol.view
 
-import com.jetbrains.life_science.section.view.ProtocolSectionView
+import com.jetbrains.life_science.section.view.SectionView
 
 data class ProtocolView(
     val id: Long,
     val name: String,
     val approachId: Long,
-    val sections: List<ProtocolSectionView>,
+    val sections: List<SectionView>,
 )
