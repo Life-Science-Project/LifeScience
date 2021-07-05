87e7af5cf8d75c4c63516cd1b193d96e0d6157bb
Kirill Peshkov <kirill906@yandex.ru>
2021-07-05 17:40:34 +0300

    Добавлена аннотация типа наследования Approach, убраны лишние импорты

--- backend/src/main/kotlin/com/jetbrains/life_science/approach/entity/Approach.kt
+++ backend/src/main/kotlin/com/jetbrains/life_science/approach/entity/Approach.kt
@@ -1,16 +1,17 @@
 package com.jetbrains.life_science.approach.entity
 
-import com.jetbrains.life_science.category.entity.Category
-import com.jetbrains.life_science.protocol.entity.Protocol
 import com.jetbrains.life_science.section.entity.Section
 import javax.persistence.ElementCollection
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.Inheritance
+import javax.persistence.InheritanceType
 import javax.persistence.OneToMany
 
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 abstract class Approach(
 
     @Id
