902df8de234df744e213a552de9e05271cae6b1b
Borodin239 <j.borodin1@mail.ru>
2021-07-06 10:08:58 +0300

    Тест подключения GitHub к YouTrack #LS-8

--- backend/src/test/kotlin/com/jetbrains/life_science/LifeScienceApplicationTests.kt
+++ backend/src/test/kotlin/com/jetbrains/life_science/LifeScienceApplicationTests.kt
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 
+// Тестовое сообщение
+
 @SpringBootTest
 class LifeScienceApplicationTests {
 
