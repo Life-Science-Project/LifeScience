1843337ee04a5f121c20ca8e42005b9819d22f24
Kirill Peshkov <kirill906@yandex.ru>
2021-07-06 14:52:43 +0300

    Удалены репозитории абстрактных классов

--- backend/src/main/kotlin/com/jetbrains/life_science/protocol/repository/ProtocolRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.jetbrains.life_science.protocol.repository
-
-import com.jetbrains.life_science.protocol.entity.Protocol
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ProtocolRepository : JpaRepository<Protocol, Long>
