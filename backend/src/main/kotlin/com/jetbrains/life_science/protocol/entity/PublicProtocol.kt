2676eae1e545fab024b935a76ef1d282564159c0
Kirill Peshkov <kirill906@yandex.ru>
2021-07-06 09:55:28 +0300

    Оптимизация импортов DraftProtocol.kt И PublicProtocol.kt

--- backend/src/main/kotlin/com/jetbrains/life_science/protocol/entity/PublicProtocol.kt
+++ backend/src/main/kotlin/com/jetbrains/life_science/protocol/entity/PublicProtocol.kt
@@ -1,6 +1,5 @@
 package com.jetbrains.life_science.protocol.entity
 
-import com.jetbrains.life_science.approach.entity.Approach
 import com.jetbrains.life_science.approach.entity.PublicApproach
 import com.jetbrains.life_science.section.entity.Section
 import com.jetbrains.life_science.user.master.entity.User
