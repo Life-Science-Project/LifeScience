ddf7ff9bc1732b1f57fab2731769d88c8733862e
Kirill Peshkov <kirill906@yandex.ru>
2021-07-06 14:05:56 +0300

    Добавлен абстрактный review request, а также review и вспомогательные enum

---
+++ backend/src/main/kotlin/com/jetbrains/life_science/review/request/entity/ReviewRequest.kt
@@ -0,0 +1,26 @@
+package com.jetbrains.life_science.review.request.entity
+
+import com.jetbrains.life_science.review.primary.entity.Review
+import com.jetbrains.life_science.user.master.entity.User
+import java.util.Date
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
+
+@Entity
+abstract class ReviewRequest(
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    val id: Long,
+
+    var date: Date,
+
+    @OneToMany
+    var reviews: MutableList<Review>,
+
+    @ManyToOne
+    var editor: User
+)
