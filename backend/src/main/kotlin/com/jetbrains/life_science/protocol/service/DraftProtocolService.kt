f2307e0ad90b510d61c72a209ab9b7ce3db05b0d
Borodin239 <j.borodin1@yandex.ru>
2021-07-09 14:51:09 +0300

    Исправлены комментарии о пробрасываемых исключениях к методам в DraftProtocolService.kt

--- backend/src/main/kotlin/com/jetbrains/life_science/protocol/service/DraftProtocolService.kt
+++ backend/src/main/kotlin/com/jetbrains/life_science/protocol/service/DraftProtocolService.kt
@@ -4,20 +4,22 @@ import com.jetbrains.life_science.protocol.entity.DraftProtocol
 import com.jetbrains.life_science.user.credentials.entity.Credentials
 
 interface DraftProtocolService {
-    // DraftProtocolNotExistException - если не существует
+    // DraftProtocolNotFoundException
     fun get(draftProtocolId: Long): DraftProtocol
 
+    // DraftApproachNotExistException - не найден approach, к которому прикрепить протокол
     fun create(protocolInfo: DraftProtocolInfo): DraftProtocol
 
     // DraftApproachNotExistException - не найден approach, к которому прикрепить протокол
+    // DraftProtocolNotFoundException
     fun update(protocolInfo: DraftProtocolInfo): DraftProtocol
 
-    // DraftApproachNotExistException - не найден approach, к которому прикрепить протокол
+    // DraftProtocolNotFoundException
     fun addParticipant(draftProtocolId: Long, credentials: Credentials)
 
-    // DraftApproachNotExistException - не найден approach, к которому прикрепить протокол
+    // DraftProtocolNotFoundException
     fun removeParticipant(draftProtocolId: Long, credentials: Credentials)
 
-    // DraftProtocolNotExistException - если не существует
+    // DraftProtocolNotFoundException
     fun delete(draftProtocolId: Long)
 }
