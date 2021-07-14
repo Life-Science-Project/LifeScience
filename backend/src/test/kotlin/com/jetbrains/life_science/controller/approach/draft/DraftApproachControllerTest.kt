d26e590af9961b0fb49b5acd03f89f700611331d
avpotapov00 <aleksandrpotapov955@gmail.com>
2021-07-15 02:52:50 +0300

    Добавлены тестовые файлы

---
+++ backend/src/test/kotlin/com/jetbrains/life_science/controller/approach/draft/DraftApproachControllerTest.kt
@@ -0,0 +1,10 @@
+package com.jetbrains.life_science.controller.approach.draft
+
+import com.jetbrains.life_science.ApiTest
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.test.context.jdbc.Sql
+
+@Sql("")
+internal class DraftApproachControllerTest : ApiTest() {
+
+}
