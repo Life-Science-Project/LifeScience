9c851b76860ef0aad00a083e82e122eb6785814e
avpotapov00 <aleksandrpotapov955@gmail.com>
2021-07-20 11:03:11 +0300

    Добавлено создание search unit

--- src/main/kotlin/com/jetbrains/life_science/approach/service/PublicApproachServiceImpl.kt
+++ src/main/kotlin/com/jetbrains/life_science/approach/service/PublicApproachServiceImpl.kt
@@ -4,13 +4,15 @@ import com.jetbrains.life_science.approach.entity.DraftApproach
 import com.jetbrains.life_science.approach.entity.PublicApproach
 import com.jetbrains.life_science.approach.factory.PublicApproachFactory
 import com.jetbrains.life_science.approach.repository.PublicApproachRepository
+import com.jetbrains.life_science.approach.search.service.ApproachSearchUnitService
 import com.jetbrains.life_science.exception.not_found.PublicApproachNotFoundException
 import org.springframework.stereotype.Service
 
 @Service
 class PublicApproachServiceImpl(
     val repository: PublicApproachRepository,
-    val factory: PublicApproachFactory
+    val factory: PublicApproachFactory,
+    val searchUnitService: ApproachSearchUnitService
 ) : PublicApproachService {
     override fun get(id: Long): PublicApproach {
         return repository.findById(id).orElseThrow {
@@ -20,6 +22,8 @@ class PublicApproachServiceImpl(
 
     override fun create(approach: DraftApproach): PublicApproach {
         val publicApproach = factory.create(approach)
-        return repository.save(publicApproach)
+        val savedPublicApproach = repository.save(publicApproach)
+        searchUnitService.createSearchUnit(savedPublicApproach)
+        return savedPublicApproach
     }
 }
