d7847f6bae63f3408ff61f246791e3147d30cd7d
Kirill Peshkov <kirill906@yandex.ru>
2021-07-20 09:56:53 +0300

    Ответственность judge разделена на 2 сервиса: judge и publisher

--- src/main/kotlin/com/jetbrains/life_science/judge/service/JudgePublishServiceImpl.kt
+++ src/main/kotlin/com/jetbrains/life_science/judge/service/JudgePublishServiceImpl.kt
@@ -1,23 +1,16 @@
 package com.jetbrains.life_science.judge.service
 
-import com.jetbrains.life_science.approach.service.DraftApproachService
-import com.jetbrains.life_science.approach.service.PublicApproachInfo
-import com.jetbrains.life_science.approach.service.PublicApproachService
 import com.jetbrains.life_science.judge.events.JudgePublishApproachApproveEvent
 import com.jetbrains.life_science.judge.events.JudgePublishApproachRejectEvent
 import com.jetbrains.life_science.judge.events.JudgePublishProtocolApproveEvent
 import com.jetbrains.life_science.judge.events.JudgePublishProtocolRejectEvent
-import com.jetbrains.life_science.protocol.service.DraftProtocolService
-import com.jetbrains.life_science.protocol.service.PublicProtocolInfo
-import com.jetbrains.life_science.protocol.service.PublicProtocolService
+import com.jetbrains.life_science.publisher.service.PublisherService
 import com.jetbrains.life_science.review.request.entity.PublishApproachRequest
 import com.jetbrains.life_science.review.request.entity.PublishProtocolRequest
 import com.jetbrains.life_science.review.request.entity.ReviewRequest
 import com.jetbrains.life_science.review.request.service.PublishApproachRequestService
 import com.jetbrains.life_science.review.request.service.PublishProtocolRequestService
 import com.jetbrains.life_science.review.response.entity.ReviewResolution
-import com.jetbrains.life_science.section.entity.Section
-import com.jetbrains.life_science.section.service.SectionService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
@@ -26,13 +19,9 @@ import javax.transaction.Transactional
 @Service
 @Transactional
 class JudgePublishServiceImpl(
-    val sectionService: SectionService,
+    val publisher: PublisherService,
     val publishApproachRequestService: PublishApproachRequestService,
     val publishProtocolRequestService: PublishProtocolRequestService,
-    val draftApproachService: DraftApproachService,
-    val publicApproachService: PublicApproachService,
-    val draftProtocolService: DraftProtocolService,
-    val publicProtocolService: PublicProtocolService,
     val applicationEventPublisher: ApplicationEventPublisher
 ) : JudgePublishService {
 
@@ -48,20 +37,7 @@ class JudgePublishServiceImpl(
         }
 
         if (canBeApproved(request)) {
-            val draftApproach = request.approach
-
-            // Create public entity
-            val publicApproach = publicApproachService.create(
-                object : PublicApproachInfo {
-                    override val approach = draftApproach
-                }
-            )
-
-            // Delete draft entity
-            draftApproachService.delete(draftApproach.id)
-
-            // Publish sections in public entity
-            publishSections(publicApproach.sections)
+            val publicApproach = publisher.publishDraftApproach(request.approach)
 
             // Delete publish request
             publishApproachRequestService.delete(request.id)
@@ -87,20 +63,7 @@ class JudgePublishServiceImpl(
         }
 
         if (canBeApproved(request)) {
-            val draftProtocol = request.protocol
-
-            // Create public entity
-            val publicProtocol = publicProtocolService.create(
-                object : PublicProtocolInfo {
-                    override val protocol = draftProtocol
-                }
-            )
-
-            // Delete draft entity
-            draftProtocolService.delete(draftProtocol.id)
-
-            // Publish sections in public entity
-            publishSections(publicProtocol.sections)
+            val publicProtocol = publisher.publishDraftProtocol(request.protocol)
 
             // Delete publish request
             publishProtocolRequestService.delete(request.id)
@@ -124,6 +87,4 @@ class JudgePublishServiceImpl(
         request.reviews.count { it.resolution == ReviewResolution.APPROVE } == approveLimit
 
     private fun canBeJudged(request: ReviewRequest) = request.reviews.size != verdictLimit
-
-    private fun publishSections(sections: List<Section>) = sections.forEach { sectionService.publish(it.id) }
 }
