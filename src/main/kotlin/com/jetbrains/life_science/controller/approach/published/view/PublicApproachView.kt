a1706b9e30433cbadd55f1c035850d01c68b5f81
Kirill Peshkov <kirill906@yandex.ru>
2021-07-21 12:34:41 +0300

    Добавлен контроллер public_approach с методом get, тесты к нему

---
+++ src/main/kotlin/com/jetbrains/life_science/controller/approach/published/view/PublicApproachView.kt
@@ -0,0 +1,15 @@
+package com.jetbrains.life_science.controller.approach.published.view
+
+import com.jetbrains.life_science.controller.category.view.CategoryShortView
+import com.jetbrains.life_science.controller.section.view.SectionShortView
+import com.jetbrains.life_science.controller.user.UserShortView
+import com.jetbrains.life_science.protocol.view.ProtocolShortView
+
+data class PublicApproachView(
+    val id: Long,
+    val name: String,
+    val categories: List<CategoryShortView>,
+    val sections: List<SectionShortView>,
+    val coAuthors: List<UserShortView>,
+    val protocols: List<ProtocolShortView>
+)
---
+++ src/main/kotlin/com/jetbrains/life_science/protocol/view/ProtocolShortView.kt
@@ -0,0 +1,6 @@
+package com.jetbrains.life_science.protocol.view
+
+data class ProtocolShortView(
+    val id: Long,
+    val name: String
+)
