0175d9385e22725f1d9098c5d956ab295ecbd67f
avpotapov00 <aleksandrpotapov955@gmail.com>
2021-07-26 20:21:42 +0300

    Добавлен метод получения публичных секций

---
+++ src/main/kotlin/com/jetbrains/life_science/controller/section/PublicSectionController.kt
@@ -0,0 +1,44 @@
+package com.jetbrains.life_science.controller.section
+
+import com.jetbrains.life_science.approach.service.PublicApproachService
+import com.jetbrains.life_science.content.publish.service.ContentService
+import com.jetbrains.life_science.controller.section.view.SectionView
+import com.jetbrains.life_science.controller.section.view.SectionViewMapper
+import com.jetbrains.life_science.exception.section.SectionNotFoundException
+import com.jetbrains.life_science.section.entity.Section
+import com.jetbrains.life_science.section.service.SectionService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/approaches/public/{approachId}/sections")
+class PublicSectionController(
+    val publicApproachService: PublicApproachService,
+    val sectionService: SectionService,
+    val contentService: ContentService,
+    val viewMapper: SectionViewMapper
+) {
+
+    @GetMapping("/{sectionId}")
+    fun getSection(
+        @PathVariable approachId: Long,
+        @PathVariable sectionId: Long,
+    ): SectionView {
+        val section = getSectionSecured(approachId, sectionId)
+        val content = contentService.findBySectionId(sectionId)
+        return viewMapper.toView(section, content?.text)
+    }
+
+    private fun getSectionSecured(
+        publicApproachId: Long,
+        sectionId: Long
+    ): Section {
+        val approach = publicApproachService.get(publicApproachId)
+        if (!approach.hasSection(sectionId)) {
+            throw SectionNotFoundException(sectionId)
+        }
+        return sectionService.getById(sectionId)
+    }
+}
