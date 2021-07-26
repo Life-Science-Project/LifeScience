aa880005cd97b23e30eec76c47475dcf6ffe0893
Borodin239 <j.borodin1@yandex.ru>
2021-07-26 18:03:34 +0300

    Исправлен тип пробрасываемого исключения в getCurrentUser

--- src/main/kotlin/com/jetbrains/life_science/controller/user/UserController.kt
+++ src/main/kotlin/com/jetbrains/life_science/controller/user/UserController.kt
@@ -2,7 +2,7 @@ package com.jetbrains.life_science.controller.user
 
 import com.jetbrains.life_science.controller.user.view.UserFullView
 import com.jetbrains.life_science.controller.user.view.UserViewMapper
-import com.jetbrains.life_science.exception.auth.InvalidCredentialsException
+import com.jetbrains.life_science.exception.not_found.UserPersonalDataNotFoundException
 import com.jetbrains.life_science.user.credentials.entity.Credentials
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
@@ -18,7 +18,10 @@ class UserController(
     fun getCurrentUser(
         @AuthenticationPrincipal user: Credentials
     ): UserFullView {
-        val userData = user.userPersonalData ?: throw InvalidCredentialsException()
+        val userData = user.userPersonalData ?: throw UserPersonalDataNotFoundException(
+            "UserData not found for " +
+                    "credentials with id ${user.id}"
+        )
         return userViewMapper.toFullView(userData)
     }
 }
