47e2da20281ffc276ebc99aa67482a3d87bcea47
Borodin239 <j.borodin1@yandex.ru>
2021-07-26 13:45:01 +0300

    Выставлено апи на получение авторизированного пользователя.

--- src/main/kotlin/com/jetbrains/life_science/controller/user/UserController.kt
+++ src/main/kotlin/com/jetbrains/life_science/controller/user/UserController.kt
@@ -1,3 +1,24 @@
 package com.jetbrains.life_science.controller.user
 
-class UserController
+import com.jetbrains.life_science.controller.user.view.UserFullView
+import com.jetbrains.life_science.controller.user.view.UserViewMapper
+import com.jetbrains.life_science.exception.auth.InvalidCredentialsException
+import com.jetbrains.life_science.user.credentials.entity.Credentials
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/users")
+class UserController(
+    val userViewMapper: UserViewMapper
+) {
+    @GetMapping("/current")
+    fun getCurrentUser(
+        @AuthenticationPrincipal user: Credentials
+    ): UserFullView {
+        val userData = user.userPersonalData ?: throw InvalidCredentialsException()
+        return userViewMapper.toFullView(userData)
+    }
+}
