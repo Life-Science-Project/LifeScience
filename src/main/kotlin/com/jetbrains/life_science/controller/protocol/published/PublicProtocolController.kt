66b9a94f133feac900767c47a19f8fbc47d4bf9c
avpotapov00 <aleksandrpotapov955@gmail.com>
2021-07-30 13:57:24 +0300

    Поправлено API по протоколам, актуализированы тесты

--- src/main/kotlin/com/jetbrains/life_science/controller/protocol/published/PublicProtocolController.kt
+++ src/main/kotlin/com/jetbrains/life_science/controller/protocol/published/PublicProtocolController.kt
@@ -3,21 +3,28 @@ package com.jetbrains.life_science.controller.protocol.published
 import com.jetbrains.life_science.container.protocol.service.PublicProtocolService
 import com.jetbrains.life_science.controller.protocol.published.view.PublicProtocolView
 import com.jetbrains.life_science.controller.protocol.published.view.PublicProtocolViewMapper
+import com.jetbrains.life_science.exception.protocol.PublicProtocolNotFoundException
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/protocols/public")
+@RequestMapping("/api/protocols/public/{approachId}/protocols")
 class PublicProtocolController(
     val publicProtocolService: PublicProtocolService,
     val viewMapper: PublicProtocolViewMapper
 ) {
 
     @GetMapping("/{protocolId}")
-    fun getProtocol(@PathVariable protocolId: Long): PublicProtocolView {
+    fun getProtocol(
+        @PathVariable approachId: Long,
+        @PathVariable protocolId: Long
+    ): PublicProtocolView {
         val protocol = publicProtocolService.get(protocolId)
+        if (protocol.approach.id != approachId) {
+            throw PublicProtocolNotFoundException("Protocol not found")
+        }
         return viewMapper.toView(protocol)
     }
 }
